3f4fff2ad08293d728cd8b6e9ad3a4d7b19f4740
Marcos Thomaz <mvnthomaz@gmail.com>
2020-01-02 18:28:47 -0300

    WIP: share whatsapp and instagram post

--- packages/share_post/ios/Classes/SwiftSharePostPlugin.swift
+++ packages/share_post/ios/Classes/SwiftSharePostPlugin.swift
@@ -39,6 +39,7 @@ public class SwiftSharePostPlugin: NSObject, FlutterPlugin, SharingDelegate, UID
             sharePostOnInstagram(url: url, result: result)
             break
         case "shareOnWhatsapp" :
+        case "shareOnWhatsappBusiness" :
             let args = call.arguments as! Dictionary<String, Any>
             let url: String = args["url"] as! String
             shareOnWhatsapp(url: url, result: result)
@@ -132,183 +133,96 @@ public class SwiftSharePostPlugin: NSObject, FlutterPlugin, SharingDelegate, UID
                 UIPasteboard.general.items = pasteBoardItems
             }
             UIApplication.shared.openURL(app)
+        } else {
+            result(FlutterError(code: "INSTAGRAM_APP_NOT_FOUND", message: "Facebook app not found", details: nil))
         }
     }
     
-    // private var documentsController: UIDocumentInteractionController = UIDocumentInteractionController()
-    
     func sharePostOnInstagram(url: String, result: @escaping FlutterResult) {
-        let kInstagramURL = "instagram://app"
-        let kUTI = "com.instagram.exclusivegram"
-        let kfileNameExtension = "instagram.igo"
-        
-        let urlImage = URL(string: url)!
-        let data = try? Data(contentsOf: urlImage)
-        let image2 = UIImage(data: data!)!
-        
-        let instagramURL = URL(string: kInstagramURL)!
-        if UIApplication.shared.canOpenURL(instagramURL) {
+        let app = URL(string: "instagram://app")!
+        if UIApplication.shared.canOpenURL(app){
+            let urlImage = URL(string: url)!
+            let data = try? Data(contentsOf: urlImage)
+            let photo = UIImage(data: data!)!
             
-            let imageData = image2.jpegData(compressionQuality: 100)
-            let jpgPath = (NSTemporaryDirectory() as NSString).appendingPathComponent(kfileNameExtension)
+            let documentsController = UIActivityViewController.init(activityItems: [photo], applicationActivities: nil)
             
-            do {
-                try imageData?.write(to: URL(fileURLWithPath: jpgPath), options: .atomic)
-            } catch {
-                print(error)
-            }
-            let fileURL = URL.init(fileURLWithPath: jpgPath)
-            
-            let documentsController = UIDocumentInteractionController()
-            documentsController.url = fileURL
-            documentsController.delegate = self
-            documentsController.uti = kUTI
-            
-            // adding caption for the image
-            // documentsController.annotation = ["InstagramCaption": "instagramCaption"]
-            DispatchQueue.main.async {
-                documentsController.presentOpenInMenu(from: (UIApplication.shared.keyWindow?.rootViewController!.view.bounds)!,
-                                                      in: (UIApplication.shared.keyWindow?.rootViewController!.view)!, animated: true)
-            }
-            
-            
-            
-            //        let instagramURL = URL(string: "instagram://app")!
-            //        if UIApplication.shared.canOpenURL(instagramURL) {
-            //            let jpgPath = (NSTemporaryDirectory() as NSString).appendingPathComponent("instagrammFotoToShareName.igo")
-            //            if let image = image2.jpegData(compressionQuality: 1.0) {
-            //                try? image.write(to: urlImage, options: Data.WritingOptions.atomic) //writeToFile(jpgPath, atomically: true)
-            //                let fileURL = NSURL.fileURL(withPath: jpgPath)
-            //                documentsController.url = fileURL
-            //                documentsController.uti = "com.instagram.exclusivegram"
-            //                let rect = CGRect.zero
-            //                documentsController.presentOpenInMenu(from: rect, in: (UIApplication.shared.keyWindow?.rootViewController!.view)!, animated: true)
-            //            }
-            //        }
-            //        else {
-            //            print("Instagram not found")
-            //        }
-            
-            //        let app = URL(string: "instagram://media?id=12345678")!
-            //        if UIApplication.shared.canOpenURL(app){
-            //
-            //            UIApplication.shared.openURL(app)
-            //        }
-            //        let urlImage = URL(string: url)!
-            //        let data = try? Data(contentsOf: urlImage)
-            //        let image = UIImage(data: data!)!
-            //
-            //        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
-            //        let checkValidation = FileManager.default
-            //        let getImagePath = paths.appending("image.igo")
-            //        try? checkValidation.removeItem(atPath: getImagePath)
-            //        let imageData =  image.jpegData(compressionQuality: 1.0)
-            //        try? imageData?.write(to: URL.init(fileURLWithPath: getImagePath), options: .atomicWrite)
-            //        var documentController : UIDocumentInteractionController!
-            //        documentController = UIDocumentInteractionController.init(url: URL.init(fileURLWithPath: getImagePath))
-            //        documentController.uti = "com.instagram.exclusivegram"
-            //
-            //        let rect = CGRect.zero
-            //
-            //        documentController.presentOptionsMenu(from: rect, in: (UIApplication.shared.keyWindow?.rootViewController!.view)!, animated: true)
+            documentsController.excludedActivityTypes = [ .postToFacebook, .postToTwitter, .postToWeibo,
+                                                          .message, .mail, .print, .copyToPasteboard,
+                                                          .assignToContact, .saveToCameraRoll, .addToReadingList,
+                                                          .postToFlickr, .postToVimeo, .postToTencentWeibo, .airDrop]
+            UIApplication.shared.keyWindow?.rootViewController?.present(documentsController, animated: true, completion: nil)
+        } else {
+            result(FlutterError(code: "INSTAGRAM_APP_NOT_FOUND", message: "Facebook app not found", details: nil))
         }
+        
+        
+        //                let urlImage = URL(string: url)!
+        //                let data = try? Data(contentsOf: urlImage)
+        //                let photo = UIImage(data: data!)!
+        //
+        //                let instagramURL = URL(string: "instagram://app")!
+        //
+        //                if UIApplication.shared.canOpenURL(instagramURL) {
+        //                    let imageData = photo.jpegData(compressionQuality: 1.0)!
+        //
+        //                    let writePath = URL(fileURLWithPath: NSTemporaryDirectory() as String).appendingPathComponent("instagram.ig")
+        //
+        //                    do {
+        //                        try imageData.write(to: writePath, options: .atomic)
+        //                        let documentsInteractionsController = UIDocumentInteractionController(url: writePath)
+        //                        documentsInteractionsController.delegate = self
+        //                        documentsInteractionsController.uti = writePath.uti
+        //                        documentsInteractionsController.presentOpenInMenu(from: CGRect.zero,
+        //                                                                          in: (UIApplication.shared.keyWindow?.rootViewController!.view)!,
+        //                                                                          animated: true)
+        //
+        //
+        //                    } catch {
+        //                        return
+        //                    }
+        //
+        //
+        //                }
+        //
     }
     
     func shareOnWhatsapp(url: String, result: @escaping FlutterResult) {
-        
-        let escapedString = url.addingPercentEncoding(withAllowedCharacters:CharacterSet.urlQueryAllowed)
-        let url  = URL(string: "whatsapp://send?text=\(String(describing: escapedString))")
-        
-        if UIApplication.shared.canOpenURL(url! as URL) {
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
-            } else {
-                // Fallback on earlier versions
+        let urlWhats = "whatsapp://app"
+        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters:CharacterSet.urlQueryAllowed) {
+            if let whatsappURL = URL(string: urlString) {
+                
+                if UIApplication.shared.canOpenURL(whatsappURL as URL) {
+                    
+                    if let image = UIImage(named: "fastfood.jpg") {
+                        if let imageData = image.jpegData(compressionQuality: 1.0) {
+                            let tempFile = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/whatsAppTmp.wai")
+                            do {
+                                try imageData.write(to: tempFile, options: .atomic)
+                                let documentInteractionController = UIDocumentInteractionController(url: tempFile)
+                                documentInteractionController.uti = "public.url"
+                                documentInteractionController.presentOpenInMenu(from: CGRect.zero, in: (UIApplication.shared.keyWindow?.rootViewController!.view)!, animated: true)
+                                
+                            } catch {
+                                print(error)
+                            }
+                        }
+                    }
+                    
+                } else {
+                    // Cannot open whatsapp
+                }
             }
         }
-        
-//        let urlImage = URL(string: url)!
-//        let data = try? Data(contentsOf: urlImage)
-//        let image = UIImage(data: data!)!
-//
-//        let urlWhats = "instagram://app"
-//        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters:CharacterSet.urlQueryAllowed) {
-//            if let whatsappURL = URL(string: urlString) {
-//
-//                if UIApplication.shared.canOpenURL(whatsappURL as URL) {
-//
-//                    guard let imageData = image.pngData() else { debugPrint("Cannot convert image to data!"); return }
-//
-//                    let tempFile = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/fitbestPhoto.igo")
-//                    do {
-//                        try imageData.write(to: tempFile, options: .atomic)
-//                        let documentInteractionController = UIDocumentInteractionController(url: tempFile)
-//                        documentInteractionController.uti = "com.instagram.exclusivegram"
-//                        documentInteractionController.presentOpenInMenu(from: CGRect.zero, in: (UIApplication.shared.keyWindow?.rootViewController!.view)!, animated: true)
-//
-//                    } catch {
-//
-//                    }
-//
-//                } else {
-//
-//                }
-//            }
-//        }
-//        https://github.com/nithinbemitk/iOS-Whatsapp-Share
-//        let urlWhats: String = "whatsapp://app"
-//        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) {
-//            if let whatsappURL = URL(string: urlString) {
-//
-//                if UIApplication.shared.canOpenURL(whatsappURL) {
-//
-//                    let urlImage = URL(string: url)!
-//                    let data = try? Data(contentsOf: urlImage)
-//
-//                    if let image = UIImage(data: data!) {
-//                        let tempFile = URL(fileURLWithPath: saveImageToDocumentDirectory(image))
-//                        do {
-//                            let documentInteractionController = UIDocumentInteractionController()
-//                            documentInteractionController.url = tempFile
-//                            documentInteractionController.uti = "net.whatsapp.image"
-//                            let rect = CGRect.zero
-//                            documentInteractionController.presentOptionsMenu(from: rect, in: (UIApplication.shared.keyWindow?.rootViewController!.view)!, animated: true)
-//                        } catch {
-//                            print(error)
-//                        }
-//                    }
-//
-//                } else {
-//                    // Cannot open whatsapp
-//                }
-//            }
-//        }
     }
     
-    func saveImageToDocumentDirectory(_ chosenImage: UIImage) -> String {
-        let directoryPath =  NSHomeDirectory().appending("/Documents/")
-        if !FileManager.default.fileExists(atPath: directoryPath) {
-            do {
-                try FileManager.default.createDirectory(at: NSURL.fileURL(withPath: directoryPath), withIntermediateDirectories: true, attributes: nil)
-            } catch {
-                print(error)
-            }
-        }
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyyMMddhhmmss"
-        
-        let filename = dateFormatter.string(from: Date()).appending(".wai")
-        let filepath = directoryPath.appending(filename)
-        let url = NSURL.fileURL(withPath: filepath)
-        do {
-            try chosenImage.jpegData(compressionQuality: 1.0)?.write(to: url, options: .atomic)
-            return "/Documents/\(filename)"
-        } catch {
-            print(error)
-            print("file cant not be save at path \(filepath), with error : \(error)");
-            return filepath
-        }
+    func shareOnNative(url: String, result: @escaping FlutterResult) {
+        let urlImage = URL(string: url)!
+        let data = try? Data(contentsOf: urlImage)
+        let photo = UIImage(data: data!)!
+        let documentsController = UIActivityViewController.init(activityItems: [photo], applicationActivities: nil)
+        documentsController.excludedActivityTypes = [ .postToFacebook ]
+        UIApplication.shared.keyWindow?.rootViewController?.present(documentsController, animated: true, completion: nil)
     }
     
     public func sharer(_ sharer: Sharing, didCompleteWithResults results: [String : Any]) {
@@ -322,6 +236,5 @@ public class SwiftSharePostPlugin: NSObject, FlutterPlugin, SharingDelegate, UID
     public func sharerDidCancel(_ sharer: Sharing) {
         
     }
-    
     
 }
